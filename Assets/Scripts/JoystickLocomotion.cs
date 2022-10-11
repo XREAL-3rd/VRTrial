12281147e7dffafaf418d289fb1066e9b0b8774e
hagipoker <hagipoker@snu.ac.kr>
2022-10-11 19:26:19 +0900

    Joystick Locomotion (Rotate)
    Left Controller Joystick으로 rotate할 수 있고, Rgith Controller Joystick으로 move할 수 있다.

--- Assets/Scripts/JoystickLocomotion.cs
+++ Assets/Scripts/JoystickLocomotion.cs
@@ -6,14 +6,27 @@ using OVR;
 public class JoystickLocomotion : MonoBehaviour
 {
     public Rigidbody player;
-    public float speed;
-    
+    public float move_speed;
+
+    public Transform rotator;
+    public float turn_speed;
+
     void Update()
     {
-        var joystickAxis = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch);
+        var RjoystickAxis = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick, OVRInput.Controller.RTouch);
         float fixedY = player.position.y;
 
-        player.position += (transform.right * joystickAxis.x + transform.forward * joystickAxis.y) * Time.deltaTime * speed;
+        player.position += (transform.right * RjoystickAxis.x + transform.forward * RjoystickAxis.y) * Time.deltaTime * move_speed;
         player.position = new Vector3(player.position.x, fixedY, player.position.z);
+
+        var LjoystickAxis = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch);
+        if (LjoystickAxis.x >= .8f)
+        {
+            player.transform.RotateAround(rotator.position, Vector3.up, turn_speed * .1f);
+        }
+        if (LjoystickAxis.x <= -.8f)
+        {
+            player.transform.RotateAround(rotator.position, Vector3.down, turn_speed * .1f);
+        }
     }
 }
