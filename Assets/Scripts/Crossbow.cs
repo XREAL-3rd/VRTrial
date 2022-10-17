f75c7776e16820de0d13b267ebaa4b250ceb87e4
younggam <nainjoo2001@naver.com>
2022-10-17 22:08:12 +0900

    과제

--- Assets/Scripts/Crossbow.cs
+++ Assets/Scripts/Crossbow.cs
@@ -1,25 +1,37 @@
 using System;
+using System.Linq;
 using Oculus.Interaction;
 using Oculus.Interaction.HandGrab;
+using Oculus.Interaction.Input;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Crossbow : MonoBehaviour
 {
     public GameObject arrow;
+    public OVRHand leftHand;
+    public OVRHand rightHand;
     public GrabInteractable grabInteractable;
     public HandGrabInteractable handGrabInteractable;
 
     public void Update()
     {
-        if (grabInteractable.State == InteractableState.Hover || grabInteractable.State == InteractableState.Select ||
-            handGrabInteractable.State == InteractableState.Hover ||
-            handGrabInteractable.State == InteractableState.Select)
+        if (grabInteractable.State is InteractableState.Hover or InteractableState.Select &&
+            OVRInput.GetDown(OVRInput.Button.One))
         {
-            if (OVRInput.GetDown(OVRInput.Button.One))
-            {
-                var projectile = Instantiate(arrow, transform.position, Quaternion.LookRotation(-transform.right));
-                projectile.GetComponent<Rigidbody>().velocity = 10 * projectile.transform.forward;
-            }
         }
+        else if (handGrabInteractable.State is InteractableState.Hover or InteractableState.Select &&
+                 handGrabInteractable.InteractorViews.Any(i =>
+                     i is HandGrabInteractor hi && hi.Hand.Handedness == Handedness.Left) &&
+                 leftHand.GetFingerPinchStrength(OVRHand.HandFinger.Index) == 1 ||
+                 rightHand.GetFingerPinchStrength(OVRHand.HandFinger.Index) == 1)
+
+        {
+        }
+        else return;
+
+
+        var projectile = Instantiate(arrow, transform.position, Quaternion.LookRotation(-transform.right));
+        projectile.GetComponent<Rigidbody>().velocity = 10 * projectile.transform.forward;
     }
 }
